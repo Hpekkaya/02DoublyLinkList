6c05370e811ed3bf8c3e40d97ec660e7fd624800
Hpekkaya <Ahpek1970@yahoo.com>
2024-03-31 11:27:53 +0300

    append method applied

---
+++ 071Append.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        print("Link List :")
+        while temp:
+            print(f" {temp.value}", end=" ")
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+        self.length += 1
+        return True
+
+
+my_dll = DoublyLinkedList(7)
+my_dll.append(11)
+my_dll.print_list()
