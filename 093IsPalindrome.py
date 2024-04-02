d98b933e10d4497cf0aab894c385392690454696
Hpekkaya <Ahpek1970@yahoo.com>
2024-04-02 05:42:35 +0300

    is palindrome method applied

---
+++ 093IsPalindrome.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def is_palindrome(self):
+        # check length
+        if self.length == 0: return False
+        # assign forw, backw and pald
+        forward = self.head
+        backward = self.tail
+        palindrome = True
+        counter, len_half = 0, self.length/2
+        # create loop forw and pald check
+        while forward and palindrome:
+            palindrome = forward.value == backward.value
+            forward = forward.next
+            backward = backward.prev
+            if counter == len_half: break
+            counter += 1
+
+        return palindrome
+
+
+
+my_dll_1 = DoublyLinkedList(1)
+my_dll_1.append(2)
+my_dll_1.append(3)
+my_dll_1.append(2)
+my_dll_1.append(1)
+
+print('my_dll_1 is_palindrome:')
+print(my_dll_1.is_palindrome())
+
+my_dll_2 = DoublyLinkedList(1)
+my_dll_2.append(2)
+my_dll_2.append(3)
+
+print('\nmy_dll_2 is_palindrome:')
+print(my_dll_2.is_palindrome())
